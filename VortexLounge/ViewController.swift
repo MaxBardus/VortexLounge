534c7b3cd83004bd43d065a68bbf58fe50db6de6
Max Bardus <maksymbardus@gmail.com>
2018-03-22 14:50:58 -0400

    Initial commit

---
+++ VortexLounge/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  VortexLounge
+//
+//  Created by Max Bardus on 2018-02-27.
+//  Copyright © 2018 GBDevs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // Label for rotation
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // rotation label
+    
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
