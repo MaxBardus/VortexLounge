6591981937ee594683dd5f9afb67009572de1e85
Max Bardus <maksymbardus@gmail.com>
2018-03-29 15:15:45 -0400

    Prototype Core
    Core of the prototype is donw

--- VortexLounge/ViewController.swift
+++ VortexLounge/ViewController.swift
@@ -16,8 +16,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // rotation label
-    
-        
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
         
         
     }
